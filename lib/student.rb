1dd153199496fb5cd031c08d7bf97868722d13e2
Donovan Jones <donovanljones94@gmail.com>
2019-10-05 15:04:54 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -14,7 +14,7 @@ class Student
   end
 
   def add_student_attributes(attributes_hash)
-    attributes_hash.each{|value_1,value_2| self.send(("#{k}="),v)}
+    attributes_hash.each{|value_1,value_2| self.send(("#{value_1}="),value_2)}
   end
 
   def self.all
